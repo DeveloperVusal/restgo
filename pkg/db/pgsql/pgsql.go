a3050745cfd76051275ba491da555de36a906a6d
DeveloperVusal <developer.vusal96@gmail.com>
2023-11-04 17:58:27 +0300

    fix: fix bugs and minor refactoring code

--- pkg/db/pgsql/pgsql.go
+++ pkg/db/pgsql/pgsql.go
@@ -2,21 +2,20 @@ package pgsql
 
 import (
 	"context"
+	"net/url"
 	"strconv"
 	"strings"
 
 	"github.com/jackc/pgx/v5"
 )
 
-func Conn(dsn string) (context.Context, *pgx.Conn, error) {
-	config, _ := pgx.ParseConfig(dsn)
+func Conn(dsn string) (*pgx.Conn, error) {
 	// config.BuildStatementCache = func(conn *pgconn.PgConn) stmtcache.Cache {
 	// 	return stmtcache.New(conn, stmtcache.ModeDescribe, 1024)
 	// }
+	db, err := pgx.Connect(context.Background(), dsn)
 
-	_db, _err := pgx.ConnectConfig(context.Background(), config)
-
-	return context.Background(), _db, _err
+	return db, err
 }
 
 type Dsn struct {
@@ -28,5 +27,5 @@ type Dsn struct {
 }
 
 func DsnBuild(dsn Dsn) string {
-	return "postgres://" + strings.Trim(dsn.Username, " ") + ":" + strings.Trim(dsn.Password, " ") + "@" + strings.Trim(dsn.Host, " ") + ":" + strconv.Itoa(dsn.Port) + "/" + strings.Trim(dsn.Database, " ")
+	return "postgres://" + strings.Trim(dsn.Username, " ") + ":" + url.QueryEscape(strings.Trim(dsn.Password, " ")) + "@" + strings.Trim(dsn.Host, " ") + ":" + strconv.Itoa(dsn.Port) + "/" + strings.Trim(dsn.Database, " ")
 }
