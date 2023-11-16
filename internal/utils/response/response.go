1256e516725ab3a9617f3bd39767ecef76f025e1
DeveloperVusal <developer.vusal96@gmail.com>
2023-11-17 01:54:50 +0300

    feat: add response utils in internal

---
+++ internal/utils/response/response.go
@@ -0,0 +1,38 @@
+package response
+
+import "encoding/json"
+
+type Status string
+
+const (
+	Success Status = "success"
+	Error          = "error"
+	Warning        = "warning"
+)
+
+type Response struct {
+	Code    byte
+	Status  Status
+	Message string
+	Result  any
+}
+
+func (response *Response) CreateResponseData() []byte {
+	if response.Result == nil {
+		marshal, _ := json.Marshal(map[string]interface{}{
+			"code":    response.Code,
+			"status":  response.Status,
+			"message": response.Message,
+		})
+		return marshal
+	}
+
+	marshal, _ := json.Marshal(map[string]interface{}{
+		"code":    response.Code,
+		"status":  response.Status,
+		"message": response.Message,
+		"result":  response.Result,
+	})
+
+	return marshal
+}
