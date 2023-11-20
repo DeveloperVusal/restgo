f361fd2d78c6a6a87b5309dd3ad429355f433026
DeveloperVusal <developer.vusal96@gmail.com>
2023-11-21 01:48:29 +0300

    perf: add query builder for storage

--- internal/storage/pgsql/pgsql.go
+++ internal/storage/pgsql/pgsql.go
@@ -1,22 +1,26 @@
 package pgsql
 
 import (
+	"apibgo/internal/domain"
 	"apibgo/internal/storage"
 	"apibgo/pkg/db/pgsql"
+	"context"
 	"fmt"
 	"os"
 	"reflect"
 	"regexp"
 	"strconv"
 
+	"github.com/doug-martin/goqu/v9"
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 )
 
 type Storage struct {
-	db *pgx.Conn
+	Db *pgx.Conn
 }
 
 func New(cfg *storage.Config, conn string) (*Storage, error) {
@@ -73,5 +77,60 @@ func New(cfg *storage.Config, conn string) (*Storage, error) {
 		}
 	}
 
-	return &Storage{db: db}, nil
+	return &Storage{Db: db}, nil
+}
+
+func (s *Storage) Create(ctx context.Context, au domain.Auth) (pgconn.CommandTag, error) {
+	ds := goqu.Insert(au.TableName()).Rows(au)
+	sql, args, _ := ds.ToSQL()
+	cmmtag, err := s.Db.Exec(ctx, sql, args...)
+
+	return cmmtag, err
+}
+
+func (s *Storage) Save(ctx context.Context, au domain.Auth, sql string, args ...interface{}) (pgconn.CommandTag, error) {
+	ds := goqu.Update(au.TableName()).Set(au).Where(goqu.L(sql, args...))
+	_sql, _args, _ := ds.ToSQL()
+	cmmtag, err := s.Db.Exec(ctx, _sql, _args...)
+
+	return cmmtag, err
+}
+
+func (s *Storage) Delete(ctx context.Context, sql string, args ...interface{}) (pgconn.CommandTag, error) {
+	au := domain.Auth{}
+	ds := goqu.Delete(au.TableName()).Where(goqu.L(sql, args...))
+	_sql, _args, _ := ds.ToSQL()
+	cmmtag, err := s.Db.Exec(ctx, _sql, _args...)
+
+	return cmmtag, err
+}
+
+func (s *Storage) Find(ctx context.Context, table string, sql string, args ...interface{}) (pgx.Rows, error) {
+	ds := goqu.From(table).Where(goqu.L(sql, args...))
+	_sql, _args, _ := ds.ToSQL()
+	rows, err := s.Db.Query(ctx, _sql, _args...)
+
+	return rows, err
+}
+
+func (s *Storage) First(ctx context.Context, table string, sql string, args ...interface{}) (pgx.Rows, error) {
+	ds := goqu.From(table).
+		Where(goqu.L(sql, args...)).
+		Limit(1).
+		Order(goqu.I("id").Asc())
+	_sql, _args, _ := ds.ToSQL()
+	rows, err := s.Db.Query(ctx, _sql, _args...)
+
+	return rows, err
+}
+
+func (s *Storage) Last(ctx context.Context, table string, sql string, args ...interface{}) (pgx.Rows, error) {
+	ds := goqu.From(table).
+		Where(goqu.L(sql, args...)).
+		Limit(1).
+		Order(goqu.I("id").Desc())
+	_sql, _args, _ := ds.ToSQL()
+	rows, err := s.Db.Query(ctx, _sql, _args...)
+
+	return rows, err
 }
