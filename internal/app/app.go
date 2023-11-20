e30c1e88a8e2b99ab2e6cc136d63ce3ab29f4016
DeveloperVusal <developer.vusal96@gmail.com>
2023-11-21 01:49:41 +0300

    fix: fix for using routes

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,16 +2,13 @@ package app
 
 import (
 	"net/http"
-	"os"
 
 	"apibgo/internal/config"
 	"apibgo/internal/storage"
-	"apibgo/internal/storage/pgsql"
 	"apibgo/internal/transport/rest"
 	"apibgo/internal/transport/rest/routes"
 
 	"apibgo/pkg/logger"
-	"apibgo/pkg/logger/feature/slog"
 	"apibgo/pkg/univenv"
 
 	"github.com/gorilla/mux"
@@ -23,25 +20,17 @@ func Run() {
 
 	cfg := config.MustLoad()
 	log := logger.Setup(cfg.Env)
-
 	dbcfgs := storage.MustLoad()
-	pg, err := pgsql.New(dbcfgs, "master")
-
-	if err != nil {
-		log.Error("failed to init storage", slog.Err(err))
-		os.Exit(1)
-	}
-	_ = pg
-
-	log.Info("starting database")
-	log.Info("starting restapi server")
 
 	_routes := []rest.Handler{
-		&routes.Auth{}, &routes.User{},
+		&routes.Auth{Config: cfg, Storage: dbcfgs}, &routes.User{},
 	}
 
 	r := mux.NewRouter()
 	rest.NewRouter(r, _routes...)
 	r.Use(mux.CORSMethodMiddleware(r))
-	http.ListenAndServe(cfg.HTTPServer.Address, r)
+
+	if err := http.ListenAndServe(cfg.HTTPServer.Address, r); err == nil {
+		log.Info("starting restapi server")
+	}
 }
